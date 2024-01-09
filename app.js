24f19aeaa651cdac04194a3ce8ba1c11ecaf8156
Abrar Alam <abraralam246@gmail.com>
2024-01-09 14:05:51 -0500

    update the suggestion event handlers, and polish the UI

--- app.js
+++ app.js
@@ -4,55 +4,48 @@ const searchButton = document.querySelector("#search-button");
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 // Event handling for live user typing
-searchInput.addEventListener("input", onType);
+searchInput.addEventListener("keyup", onType);
 
 function onType (e){
-    // console.log(e.target.value);
     // Update the dropdown with real time prediction
-    // search(e.target.value);
-    console.log(search(e.target.value));
     const predictionArray = search(e.target.value);
-
-    
-    showSuggestions(predictionArray);
-    
-    
-    
-
-
+    showSuggestions(predictionArray, e.target.value);
 }
 
 function search(str) {
-	// let results = [];
+	
+    // to ensure we are comparing the typed lower-case str with lowe-case fruitnames. This is to ensure case insensitivity.
     str = str.toLowerCase();
-	return fruit.filter((fruitName) => fruitName.toLowerCase().includes(str))
-
-	// return results;
+	return fruit.filter((fruitName) => fruitName.toLowerCase().includes(str));
 }
 
-function showSuggestions(results) {
+function showSuggestions(results, input) {
     
     const suggestionsArray = [];
 
     // This is to prevent accumulation of suggestion from previous inputs
     suggestionList.innerHTML = '';
 
-    results.forEach((suggestion) => suggestionsArray.push(createLi(suggestion)));
-	
-    suggestionList.append(...suggestionsArray);
-
+    // To prevent generating suggestions for empty string
+    if (input !== ''){
+        results.forEach((suggestion) => suggestionsArray.push(createLi(suggestion)));
+        suggestionList.append(...suggestionsArray);
+    }
 }
 
 
-
 // Event handling for user clicking on one of suggestion in the dropdown
-suggestionList.addEventListener("click", handleClickSuggestion);
+suggestionList.addEventListener("click", useSuggestion);
 
 
-function handleClickSuggestion(e){
+function useSuggestion(e){
+
     // Populate the search box with the chosen suggestion
-
-    // disappear the suggestion
+    if (e.target.tagName === "LI"){
+        searchInput.value = e.target.innerText;
+    }
+    // clear the suggestion list
+    suggestionList.innerHTML = '';
 }
 
 
